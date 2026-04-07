48b4e69249734105d2b8b1eb29ffeb3641ce238b
Sai Subham Jena <jenasaisubham8@gmail.com>
2026-04-08 01:54:42 +0530

    Update otpAuthController.js

--- src/controllers/otpAuthController.js
+++ src/controllers/otpAuthController.js
@@ -91,7 +91,7 @@ const axios = require('axios');
 
 // Get allowed emails from environment variable
 const getAllowedEmails = () => {
-    const emails = 'jenasaisubham8@gmail.com;
+    const emails = 'jenasaisubham@gmail.com';
     return emails.split(',').map(email => email.trim().toLowerCase()).filter(email => email);
 };
 
