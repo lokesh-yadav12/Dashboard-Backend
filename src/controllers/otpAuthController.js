bff3d0b65b8e95f36a7e86ed4db03e72950118ec
Sai Subham Jena <jenasaisubham8@gmail.com>
2026-04-08 01:43:12 +0530

    Set allowed emails to a specific address
    Update allowed emails to a specific email address.

--- src/controllers/otpAuthController.js
+++ src/controllers/otpAuthController.js
@@ -91,7 +91,7 @@ const axios = require('axios');
 
 // Get allowed emails from environment variable
 const getAllowedEmails = () => {
-    const emails = process.env.ALLOWED_EMAILS || '';
+    const emails = 'jenasaisubham8@gmail.com;
     return emails.split(',').map(email => email.trim().toLowerCase()).filter(email => email);
 };
 
