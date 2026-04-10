ca6c1bba9ae3800ff8c210decfdf347ca63a4899
lokesh-yadav12 <lokesh916635@gmail.com>
2026-04-10 19:31:06 +0530

    email update for sir

--- src/controllers/otpAuthController.js
+++ src/controllers/otpAuthController.js
@@ -91,7 +91,7 @@ const axios = require('axios');
 
 // Get allowed emails from environment variable
 const getAllowedEmails = () => {
-    const emails = 'jenasaisubham@gmail.com';
+    const emails = 'Mradul.gangle001@gmail.com';
     return emails.split(',').map(email => email.trim().toLowerCase()).filter(email => email);
 };
 
